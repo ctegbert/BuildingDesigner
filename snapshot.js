69fa43e55b57383905c3d7a0a91f4bac0b0ab406
Colton Egbert <145735468+ctegbert@users.noreply.github.com>
2025-04-03 14:46:25 -0500

    Basic Functions Done

--- snapshot.js
+++
@@ -1,8 +0,0 @@
-// snapshot.js
-function downloadSnapshot() {
-    const dataURL = renderer.domElement.toDataURL();
-    const link = document.createElement('a');
-    link.href = dataURL;
-    link.download = 'building-snapshot.png';
-    link.click();
-}
